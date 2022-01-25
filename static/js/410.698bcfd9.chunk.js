73693acb4bc807989b096bddc287632d1ef01619
luserra <luciaserra.ls@gmail.com>
2022-01-25 18:30:23 -0300

    Updates

---
+++ static/js/410.698bcfd9.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunktest_25w=self.webpackChunktest_25w||[]).push([[410],{6410:function(s,c,e){e.r(c),e.d(c,{default:function(){return t}});e(2791);var n=e(184),t=function(){return(0,n.jsx)("div",{className:"form-content-success container",children:(0,n.jsx)("div",{className:"form-success",children:(0,n.jsx)("h5",{className:"form-success-title",children:"Thank you for contact us!"})})})}}}]);
+//# sourceMappingURL=410.698bcfd9.chunk.js.map
